54b8f649fc679df83da2e1ff499004897c11cace
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2022-11-15 19:19:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Readingfiles
+{
+
+
+    public static void Main()
+    {
+        StreamReader src = new StreamReader("Data.txt");
+
+        while (!src.EndOfStream)
+        {
+            string line = src.ReadLine();
+            char[] charArr = line.ToCharArray();
+            string compressedLine; 
+            int numOfRep = 0;
+            char c;
+            List<char> chars = new List<char>();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                c = charArr[i];
+                chars.Add(charArr[i]);
+
+                if (chars[i] == charArr[i])
+                {
+                  numOfRep++;
+
+                }
+            }
+            //Console.WriteLine(numOfRep + c);
+        }
+
+    }
+}
